a99c8a9aba135388e2bd978d7e4f8d40dd06a193
Shayne Rushton <mshaynerush@gmail.com>
2020-02-20 14:20:15 -0800

    Add files via upload

--- modal.js
+++ modal.js
@@ -4,27 +4,30 @@
 
 $(document).ready(function(){
 
-            $('.close-btn').click(function(){
 
-            $('.modal-box').fadeOut(250);
-            $('.modal-text').fadeOut(250)
-            $('.section-left').fadeOut(250);
-        })
-            
+  
+        var page = $('.heading').text().toLowerCase() + ".txt"
+ 
+        $('.modal-text .sect').load(page).fadeIn(500,"linear");
+        
         $('.modal-text .close-btn').click(function(){
-        $('.modal-box').fadeOut(250);
-        $('.modal-text').fadeOut(250);
-        $('.section-left').fadeOut(50);
+        $('.modal-box').fadeOut(500, "linear");
+        $('.modal-text').fadeOut(500, "linear");
+        $('.section-left').fadeOut(500, "linear");
 
         })
+
+        
 
         $('a').click(function(){
             
-            var page = $(this).attr("id") + ".txt"
-            $('.sect').load(page)
-         
-           
+            var content = $(this).attr('id') + ".txt"
+            $('.modal-text .sect').load(content).fadeIn(500,"linear")
+
   
         })
- 
+
+            
+   
+
     })
