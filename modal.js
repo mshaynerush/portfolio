90fc425e0f9b0ea4b8e0f0ea299e27cfc0d5c593
Shayne Rushton <mshaynerush@gmail.com>
2020-04-22 18:15:00 -0700

    Add files via upload

--- modal.js
+++ modal.js
@@ -3,33 +3,66 @@
 
 
 $(document).ready(function(){
-
-    $('.close-btn').fadeIn(500, "linear");
-    $('.section-left').fadeIn(500, 'linear');
-   
-        var page = $('.heading').text().toLowerCase() + ".txt"
-        
-        $('.modal-text .sect').load(page).fadeIn(500,"linear");
-
-        $('.close-btn').click(function(){
-        $('.modal-box').fadeOut(500, "linear");
-        $('.modal-text').fadeOut(500, "linear");
-        $('.section-left').fadeOut(500, "linear");
-        $('.modal-text .sect').empty();
-        $('.close-btn').fadeOut(500, "linear")
-        })
-
-        
-
+ 
         $('a').click(function(){
             
-            var content = $(this).attr('id') + ".txt"
-            $('.modal-text .sect').load(content).fadeIn(500,"linear")
-            $('.heading').text($(this).text())
+            var page = this.id + ".txt"
+           
+            $('.page-content').load(page)
+            if($('.side-by-side').offset().left >= 0){
+                
+    $('.side-by-side').animate(
+        {
+           
+            left: '-1800px'
             
+            }, { duration: 500,
+                easing: 'linear'
+                });
+    $('.page-info').animate({
+        width: '100%',
+        left: '0',
+        display: 'block'
+    }, { duration: 500 }
+    )
+    
+    $('.section-close-button').css('display', "block")
+        
+        } else {
+            $('.side-by-side').animate({
+                left: '0px',
+            }, { duration: 500,
+                easing: 'linear' })
+        }
+        
+    })
+    $('.section-close-button').click(function(){
+        $('.section-close-button').css('display', "none")
+        $('.page-info').animate({
+            left: '100%',
+            height: '100%',
+            width: '0',
+            display: 'none'
+        }, { duration: 500 })
+        $('.side-by-side').animate({
+            left: '0'
+           
+        }, {
+            duration: 500,
+            easing: 'linear'
         })
+        
+    
+            
+            setTimeout(() =>{
+                $('.page-content').empty()
+            }, 400)
+        
+
+        
 
     
-   
 
+    
     })
+})
